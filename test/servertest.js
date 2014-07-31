e6a78e0bcfa88b1089677eeb91b40993401e8564
unknown <sagar.chandarana123@gmail.com>
2014-08-01 01:35:03 +0530

    data is not an array-error

--- test/servertest.js
+++ test/servertest.js
@@ -4,6 +4,15 @@
 
 path='http://lol.sagar.appbase.io/lol/rofl'
 vertex = {prop:'val'}
-Appbase.debug.ab.network.properties.patch(path,vertex,undefined,callback=function(error,vertex){console.log(vertex)})
-Appbase.debug.ab.network.properties.get(path,{},callback);
+timestamp = 0;
 
+Appbase.debug.ab.network.properties.listenUseful(path,function(error,vertex){
+    console.log(error,vertex);
+},function(error,vertex){
+    console.log(error,vertex);
+});
+
+
+data = ['prop'];
+
+ab.network.properties.remove(path,data,false,function(error,vertex){console.log('d',error,vertex)})
